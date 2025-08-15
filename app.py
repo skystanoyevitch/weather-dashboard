bc685b4a31fda8e5aff2dafd877b3d4ee939cb40
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-08-15 11:04:40 -0700

    Replace HTTP server implementation with a Flask application for a weather dashboard

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+from flask import Flask
+app = Flask(__name__)
 
-class setupWebServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('Hello Server World')
+@app.route('/')
+def home():
+    return "Weather Dashboard coming soon!"
+
+app.run(debug=True, port=3000)
 
 
-if __name__ == "__main__":
-    hostName = "localhost"
-    serverPort = 8081
-    webServer = HTTPServer((hostName, serverPort), SimpleHTTPRequestHandler)
-    print(f"Server started http://{hostName}:{serverPort}")
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    webServer.server_close()
-    print("Server Stopped")
+
+
+
+
+
+# from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+
+# class setupWebServer(BaseHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'text/html')
+#         self.end_headers()
+#         self.wfile.write('Hello Server World')
+
+
+# if __name__ == "__main__":
+#     hostName = "localhost"
+#     serverPort = 8081
+#     webServer = HTTPServer((hostName, serverPort), SimpleHTTPRequestHandler)
+#     print(f"Server started http://{hostName}:{serverPort}")
+#     try:
+#         webServer.serve_forever()
+#     except KeyboardInterrupt:
+#         pass
+#     webServer.server_close()
+#     print("Server Stopped")
