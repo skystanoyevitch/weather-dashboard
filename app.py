7e1863269c8ea3e882eef892c3227d64f34b666c
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-08-15 10:22:32 -0700

    Add a basic HTTP server implementation in app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+
+class setupWebServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('Hello Server World')
+
+
+if __name__ == "__main__":
+    hostName = "localhost"
+    serverPort = 8081
+    webServer = HTTPServer((hostName, serverPort), SimpleHTTPRequestHandler)
+    print(f"Server started http://{hostName}:{serverPort}")
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
+    print("Server Stopped")
